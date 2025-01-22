38feea345ef29f7aa93408c84ad255856dbfd8a7
Bret <Bretren2013@gmail.com>
2025-01-21 21:27:58 -0800

    完善注册然后添加注释

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express'; // 默认导入 Express
 import connToDb from './db.js';
 import { hashPassword } from "./bcrypt.js"
+import { isValidEmail } from './tool.js';
 const app = express();
 
 
@@ -17,22 +18,43 @@ app.get('/', (req, res) => {
 });
 
 app.post('/api/reg', async (req,res) => {
-    const { username, password } = req.body;
+    // 获取 POST 过来的 username 和 password
+    const { username, password , email} = req.body;
 
-    //
+    //验证
+    if (!username || !password || !email) {
+        res.status(400)
+        res.json({
+            "error":'username or password or email are required',
+            "code":400
+        });
+        return;
+    }else if(!isValidEmail(email)) {
+        res.status(400)
+        res.json({
+            "error":'email are required',
+            "code":400
+        });
+        return;
+    }
 
 
-
+    // 选择集合
     const collection = db.collection('users');
+    //哈希密码
     const safe_password = await hashPassword(password)
+    //插入
     const result = await collection.insertOne({
         username: username,
         passsword : safe_password
     })
+    //调试
     console.log("插入成功:", result.insertedId);
     console.log(safe_password);
+
+    // 返回
     res.status(200)
-    res.send(req.body);
+    res.json({ data: req.body, code: 200});
 })
 
 app.listen(3000, () => {
